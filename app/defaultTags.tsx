32e6b4c313e1f925d4f17659c20aca3b084d6306
JuanseChavero <ch4v398@gmail.com>
2023-01-21 14:02:38 -0300

    Add defaultTags shared component

---
+++ app/defaultTags.tsx
@@ -0,0 +1,37 @@
+export default function DefaultTags() {
+  return (
+    <>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, minimum-scale=1.0"
+      />
+      <meta name="author" content="Juan Segundo Chavero" />
+      <meta
+        name="keywords"
+        content="juan segundo chavero, juan s. chavero, juanse chavero, JuanseChavero, software developer, programmer, coding, react, javascript, typescript, nextjs, redux, nodejs, express, flutter, dart, mongodb, mern stack."
+      />
+      <meta property="og:title" content="Juan S. Chavero | Portfolio" />
+      <meta
+        property="og:description"
+        content="I am a software developer with a passion for creating elegant, efficient, and user-friendly solutions. My skills include expertise in various programming languages and frameworks such as React, Flutter, NextJS, NodeJS, Express, Typescript and more."
+      />
+      <meta property="og:url" content="https://juansegundochavero.vercel.app" />
+      <meta
+        property="og:image"
+        content="https://juansegundochavero.vercel.app/images/logos/logo.svg"
+      />
+      <meta
+        property="og:image:secutre_url"
+        content="https://juansegundochavero.vercel.app/images/logos/logo.svg"
+      />
+      <meta property="og:locale" content="en" />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:site_name"
+        content="Juan Segundo Chavero | Software Developer"
+      />
+      <meta name="robots" content="follow, index" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
