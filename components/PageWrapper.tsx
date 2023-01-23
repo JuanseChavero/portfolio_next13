71cdda3aa2c0b4fe0afec9d0bcf7c4dec479ac22
JuanseChavero <ch4v398@gmail.com>
2023-01-23 16:49:46 -0300

    Change overflow-y to hidden on pagewrapper

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -9,6 +9,11 @@ interface PageWrapperProps {
   className?: string;
 }
 
+/**
+ * Class "min-h-[calc(screenHeight, navbarHeight, footerHeight)]"
+ * is used to made the body take always the height of the screen.
+ */
+
 export default function PageWrapper({ children, className }: PageWrapperProps) {
   const path = usePathname();
 
@@ -20,7 +25,7 @@ export default function PageWrapper({ children, className }: PageWrapperProps) {
         // animate={{ opacity: 1, y: 0 }}
         // exit={{ opacity: 0, y: 50 }}
         // transition={{ delay: 1 }}
-        className={`flex min-h-[calc(100vh-128px-124px)] flex-col items-center justify-center px-8 md:px-24 lg:min-h-[calc(100vh-128px-72px)] lg:px-24 xl:px-0 ${className}`}
+        className={`flex min-h-[calc(100vh-120px-124px)] flex-col items-center justify-center overflow-y-hidden px-8 md:px-24 lg:min-h-[calc(100vh-120px-72px)] lg:px-24 xl:px-0 ${className}`}
       >
         {children}
       </motion.div>
