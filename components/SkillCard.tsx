367684cc6adf8ce5c59ad516f2da34a93a268ef6
JuanseChavero <ch4v398@gmail.com>
2023-01-26 21:09:41 -0300

    Add transition to whileHover

--- components/SkillCard.tsx
+++ components/SkillCard.tsx
@@ -5,7 +5,11 @@ export default function SkillCard({ title, icon }: Skill) {
   return (
     <motion.div
       className="relative flex flex-col items-center justify-center w-full gap-6 p-6 text-center border rounded shadow-md cursor-default border-primary bg-neutral-50 dark:bg-neutral-900"
-      whileHover={{ scale: 1.05, zIndex: 30 }}
+      whileHover={{
+        scale: 1.05,
+        zIndex: 30,
+      }}
+      transition={{ scale: { duration: 0.3 } }}
     >
       <div className="mt-2">{icon}</div>
       <span className="text-base text-gray-500 uppercase dark:text-gray-400">
