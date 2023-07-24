e07804f22c2fde98d602a5b929375a84521e7a06
JuanSegundoChavero <jchavero@latechfactory.com>
2023-07-23 21:43:15 -0300

    Move new experiences to the top

--- data/experiences.tsx
+++ data/experiences.tsx
@@ -11,20 +11,6 @@ export type Experience = {
 
 export const experiences: Experience[] = [
   {
-    company: "Tesys",
-    companySite: "https://www.tesysweb.com",
-    startDate: "May 2021",
-    endDate: "May 2022",
-    role: "Frontend lead",
-    modality: "On-site",
-    time: "Full-time",
-    contents: [
-      "Worked daily with a team of four people to build a complex managment system for a specific type of company.",
-      "Developed primarly with Flutter and Dart, aiming for a multi-platform experience.",
-      "Improved the design of already existing websites, using plain HTML, CSS and JavaScript.",
-    ],
-  },
-  {
     company: "Latech",
     companySite: "https://latechfactory.com",
     startDate: "May 2023",
@@ -36,6 +22,20 @@ export const experiences: Experience[] = [
       "Working remotely in a project with a custom-made Shopify framework, using JavaScript/TypeScript and React.",
       "Creating new features, developing new websites and mantaining projects with a team.",
       "Cooperating closely with the client's team to develop the best possible product.",
+    ],
+  },
+  {
+    company: "Tesys",
+    companySite: "https://www.tesysweb.com",
+    startDate: "May 2021",
+    endDate: "May 2022",
+    role: "Frontend lead",
+    modality: "On-site",
+    time: "Full-time",
+    contents: [
+      "Worked daily with a team of four people to build a complex managment system for a specific type of company.",
+      "Developed primarly with Flutter and Dart, aiming for a multi-platform experience.",
+      "Improved the design of already existing websites, using plain HTML, CSS and JavaScript.",
     ],
   },
 ];
