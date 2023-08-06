3a0845d39acc8741ccf8761274a2594cc7c4b604
JuanSegundoChavero <jchavero@latechfactory.com>
2023-08-05 22:32:18 -0300

    Add pagination to work experiences

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -1,4 +1,4 @@
-import { Experience } from "@/data/experiences";
+import { CompanyExperience } from "@/data/experiences";
 import {
   AnimatePresence,
   LayoutGroup,
@@ -7,9 +7,11 @@ import {
   Variants,
 } from "framer-motion";
 import { useState } from "react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
+import Tooltip from "./Tooltip";
 
 interface WorkExperienceProps {
-  experiences: Experience[];
+  experiences: CompanyExperience[];
   variants?: Variants;
 }
 
@@ -17,14 +19,46 @@ export default function WorkExperience({
   experiences,
   variants,
 }: WorkExperienceProps) {
-  const [active, setActive] = useState(experiences[0].company);
-  const controls = useAnimationControls();
-  const selectedExperience = experiences.find(
-    (experience) => experience.company === active
+  const [activeCompany, setActiveCompany] = useState(experiences[0].company);
+  const [activeExperience, setActiveExperience] = useState(
+    experiences[0].experience[0].startDate
   );
+  const selectedCompany =
+    experiences.find((experience) => experience.company === activeCompany) ??
+    experiences[0];
+  const totalExperiences = selectedCompany.experience.length;
+  const selectedExperience = selectedCompany.experience.find(
+    (experience) => experience.startDate === activeExperience
+  );
+  const experienceIndex = selectedCompany.experience.findIndex(
+    (experience) => experience.startDate === activeExperience
+  );
+  const controls = useAnimationControls();
+
+  const previousExperience = () => {
+    const canGoBack = experienceIndex > 0;
+    if (!selectedCompany || !canGoBack) return;
+    setActiveExperience(
+      selectedCompany.experience[experienceIndex - 1].startDate
+    );
+  };
+
+  const nextExperience = () => {
+    const canGoFoward = experienceIndex < totalExperiences - 1;
+    if (!selectedCompany || !canGoFoward) return;
+    setActiveExperience(
+      selectedCompany.experience[experienceIndex + 1].startDate
+    );
+  };
 
   const onClick = async (company: string) => {
-    setActive(company);
+    const clickedCompany = experiences.find(
+      (experience) => experience.company === company
+    );
+    if (!clickedCompany) return;
+
+    setActiveCompany(company);
+    setActiveExperience(clickedCompany.experience[0].startDate);
     await controls.start({ opacity: 0 });
     await controls.start({ opacity: 1 });
   };
@@ -34,9 +68,9 @@ export default function WorkExperience({
       className="container flex flex-col max-w-2xl rounded-md text-start lg:flex-row"
       variants={variants}
     >
-      {/* Works List */}
-      <div className="">
-        <ul className="flex flex-row overflow-x-scroll text-center scrollbar lg:w-40 lg:flex-col lg:overflow-x-clip">
+      {/* Companies List */}
+      <div className="w-auto">
+        <ul className="flex flex-row overflow-x-auto text-center scrollbar lg:w-40 lg:flex-col lg:overflow-x-clip">
           <LayoutGroup>
             {experiences.map(({ company }) => (
               <m.li
@@ -45,8 +79,9 @@ export default function WorkExperience({
                 className="relative flex-1 p-4 rounded cursor-pointer hover:bg-black/10 dark:hover:bg-white/10"
               >
                 {company}
+                {/* Navigation active bar */}
                 <AnimatePresence>
-                  {selectedExperience?.company === company && (
+                  {selectedCompany?.company === company && (
                     <m.div
                       key={company}
                       layoutId="company"
@@ -57,8 +92,9 @@ export default function WorkExperience({
                     />
                   )}
                 </AnimatePresence>
+                {/* Navigation scroll background */}
                 <div className="absolute top-0 bottom-0 right-0 hidden w-1 h-full -z-20 bg-gray-500/50 lg:block" />
-                <div className="absolute bottom-0 left-0 right-0 block w-full h-1 -z-20 bg-white/20 lg:hidden" />
+                <div className="absolute bottom-0 left-0 right-0 block w-full h-1 bg-gray-200 dark:bg-gray-500 -z-20 lg:hidden" />
               </m.li>
             ))}
           </LayoutGroup>
@@ -67,69 +103,112 @@ export default function WorkExperience({
 
       {/* Selected Experience */}
       <div className="flex-1">
-        {selectedExperience ? (
+        {selectedCompany && selectedExperience ? (
           <m.article
             className="flex flex-col px-1 py-4 lg:px-6 lg:py-0"
             initial={false}
             animate={controls}
             transition={{ opacity: { duration: 0.2 } }}
           >
-            <div className="flex items-start gap-6 pb-3">
-              <div className="">
-                <span className="text-sm text-gray-500 dark:text-gray-400">
-                  Company
-                </span>
-                <p className="text-xl font-bold text-primary">
-                  <m.a
-                    href={selectedExperience.companySite}
-                    target="_blank"
-                    rel="noreferrer"
-                    aria-label={`Link to ${selectedExperience.company} website`}
-                    className="flex items-start font-header hover:text-black dark:hover:text-white"
-                  >
-                    {selectedExperience.company}
-                  </m.a>
-                </p>
+            {/* Experience information */}
+            <div className="flex flex-col sm:flex-row">
+              <div className="flex flex-col items-start flex-1 order-2 sm:order-1">
+                <div className="flex gap-6">
+                  <div className="w-auto">
+                    <span className="text-sm text-gray-500 dark:text-gray-400">
+                      Company
+                    </span>
+                    <p className="text-xl font-bold text-primary">
+                      <m.a
+                        href={selectedCompany.companySite}
+                        target="_blank"
+                        rel="noreferrer"
+                        aria-label={`Link to ${selectedCompany.company} website`}
+                        className="flex items-start font-header hover:text-black dark:hover:text-white"
+                      >
+                        {selectedCompany.company}
+                      </m.a>
+                    </p>
+                  </div>
+                  <div className="w-auto">
+                    <span className="text-sm text-gray-500 dark:text-gray-400">
+                      Date range
+                    </span>
+                    <p className="text-base font-medium text-gray-700 dark:text-gray-300">
+                      {selectedExperience.startDate} -{" "}
+                      {selectedExperience.endDate}
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-wrap gap-6">
+                  <div className="w-auto">
+                    <span className="text-sm text-gray-500 dark:text-gray-400">
+                      Role
+                    </span>
+                    <p className="text-base font-medium text-gray-700 dark:text-gray-300">
+                      {selectedExperience.role}
+                    </p>
+                  </div>
+                  <div className="w-auto">
+                    <span className="text-sm text-gray-500 dark:text-gray-400">
+                      Modality
+                    </span>
+                    <p className="text-base font-medium text-gray-700 dark:text-gray-300">
+                      {selectedExperience.modality ?? "-"}
+                    </p>
+                  </div>
+                  <div className="w-auto">
+                    <span className="text-sm text-gray-500 dark:text-gray-400">
+                      Workload
+                    </span>
+                    <p className="text-base font-medium text-gray-700 dark:text-gray-300">
+                      {selectedExperience.time ?? "-"}
+                    </p>
+                  </div>
+                </div>
               </div>
-              <div className="">
-                <span className="text-sm text-gray-500 dark:text-gray-400">
-                  Date range
-                </span>
-                <p className="text-base font-medium text-gray-700 dark:text-gray-300">
-                  {selectedExperience.startDate} - {selectedExperience.endDate}
-                </p>
-              </div>
+
+              {/* Company experiences navigation */}
+              {totalExperiences > 1 && (
+                <div className="flex items-center order-1 gap-1 h-fit sm:order-2">
+                  <Tooltip content="Previous" side="bottom">
+                    <button
+                      aria-label="Previous experience"
+                      onClick={previousExperience}
+                      disabled={experienceIndex <= 0}
+                      className="flex items-center justify-center w-10 h-10 p-1 rounded-full group hover:bg-white/10 disabled:hover:bg-transparent"
+                    >
+                      <ChevronLeftIcon className="w-8 h-8 -ml-0.5 fill-primary group-disabled:fill-gray-400 dark:group-disabled:fill-gray-500" />
+                    </button>
+                  </Tooltip>
+                  <span className="flex gap-1 text-sm text-gray-400 cursor-default dark:text-gray-300">
+                    <span
+                      aria-label="Current experience number"
+                      className="text-secondary"
+                    >
+                      {experienceIndex + 1}
+                    </span>
+                    <span>/</span>
+                    <span aria-label="Total experiences">
+                      {totalExperiences}
+                    </span>
+                  </span>
+                  <Tooltip content="Next" side="bottom">
+                    <button
+                      aria-label="Next experience"
+                      onClick={nextExperience}
+                      disabled={experienceIndex >= totalExperiences - 1}
+                      className="flex items-center justify-center w-10 h-10 p-1 rounded-full group hover:bg-white/10 disabled:hover:bg-transparent"
+                    >
+                      <ChevronRightIcon className="w-8 h-8 -mr-0.5 fill-primary group-disabled:fill-gray-400 dark:group-disabled:fill-gray-500" />
+                    </button>
+                  </Tooltip>
+                </div>
+              )}
             </div>
 
-            <div className="flex flex-wrap items-start gap-6">
-              <div className="w-auto">
-                <span className="text-sm text-gray-500 dark:text-gray-400">
-                  Role
-                </span>
-                <p className="text-base font-medium text-gray-700 dark:text-gray-300">
-                  {selectedExperience.role}
-                </p>
-              </div>
-
-              <div className="w-auto">
-                <span className="text-sm text-gray-500 dark:text-gray-400">
-                  Modality
-                </span>
-                <p className="text-base font-medium text-gray-700 dark:text-gray-300">
-                  {selectedExperience.modality ?? "-"}
-                </p>
-              </div>
-
-              <div className="w-auto">
-                <span className="text-sm text-gray-500 dark:text-gray-400">
-                  Workload
-                </span>
-                <p className="text-base font-medium text-gray-700 dark:text-gray-300">
-                  {selectedExperience.time ?? "-"}
-                </p>
-              </div>
-            </div>
-            <div className="mt-4">
+            {/* Experience content */}
+            <div className="mt-4 min-h-[13rem]">
               <p className="mb-3 text-xl font-semibold text-primary">
                 {selectedExperience.endDate === "Today"
                   ? "What I'm doing"
--- data/experiences.tsx
+++ data/experiences.tsx
@@ -1,6 +1,10 @@
-export type Experience = {
+export type CompanyExperience = {
   company: string;
   companySite?: string;
+  experience: Experience[];
+};
+
+export type Experience = {
   startDate: string;
   endDate: string;
   role: string;
@@ -9,33 +13,53 @@ export type Experience = {
   contents: string[];
 };
 
-export const experiences: Experience[] = [
+export const experiences: CompanyExperience[] = [
   {
     company: "Latech",
     companySite: "https://latechfactory.com",
-    startDate: "May 2023",
-    endDate: "Today",
-    role: "Fullstack Developer",
-    modality: "Remote",
-    time: "Full-time",
-    contents: [
-      "Working remotely in a project with a custom-made Shopify framework, using JavaScript/TypeScript and React.",
-      "Creating new features, developing new websites and mantaining projects with a team.",
-      "Cooperating closely with the client's team to develop the best possible product.",
+    experience: [
+      {
+        startDate: "May 2023",
+        endDate: "Today",
+        role: "Fullstack Developer",
+        modality: "Remote",
+        time: "Full-time",
+        contents: [
+          "Working remotely in a project with a custom-made Shopify framework, using JavaScript/TypeScript and React.",
+          "Creating new features, developing new websites and mantaining projects with a team.",
+          "Cooperating closely with the client's team to develop the best possible product.",
+        ],
+      },
     ],
   },
   {
     company: "Tesys",
     companySite: "https://www.tesysweb.com",
-    startDate: "May 2021",
-    endDate: "May 2022",
-    role: "Frontend lead",
-    modality: "On-site",
-    time: "Full-time",
-    contents: [
-      "Worked daily with a team of four people to build a complex managment system for a specific type of company.",
-      "Developed primarly with Flutter and Dart, aiming for a multi-platform experience.",
-      "Improved the design of already existing websites, using plain HTML, CSS and JavaScript.",
+    experience: [
+      {
+        startDate: "May 2021",
+        endDate: "May 2022",
+        role: "Frontend lead",
+        modality: "On-site",
+        time: "Full-time",
+        contents: [
+          "Worked daily with a team of four people to build a complex managment system for a specific type of company.",
+          "Developed primarly with Flutter and Dart, aiming for a multi-platform experience.",
+          "Improved the design of already existing websites, using plain HTML, CSS and JavaScript.",
+        ],
+      },
+      {
+        startDate: "Feb 2023",
+        endDate: "May 2023",
+        role: "Frontend developer",
+        modality: "Remote",
+        time: "Full-time",
+        contents: [
+          "Implemented a thorough API structure refactor to improve app security.",
+          "Diligently maintained and supported the app.",
+          "Continuously enhanced the app to optimize the user experience.",
+        ],
+      },
     ],
   },
 ];
