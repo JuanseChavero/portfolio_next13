49d2164b98c7aeb14f62408defc773b0bdd15145
JuanseChavero <juansechavero@gmail.com>
2024-01-06 19:37:50 -0300

    refactor: rewrite bullet points on experiences

--- data/experiences.tsx
+++ data/experiences.tsx
@@ -6,7 +6,7 @@ export type CompanyExperience = {
 
 export type Experience = {
   startDate: string;
-  endDate: string;
+  endDate?: string;
   role: string;
   modality?: string;
   time?: string;
@@ -20,14 +20,13 @@ export const experiences: CompanyExperience[] = [
     experience: [
       {
         startDate: "May 2023",
-        endDate: "Today",
         role: "Fullstack Developer",
         modality: "Remote",
         time: "Full-time",
         contents: [
-          "Working remotely in a project with a custom-made Shopify framework, using JavaScript/TypeScript and React.",
-          "Creating new features, developing new websites and mantaining projects with a team.",
-          "Cooperating closely with the client's team to develop the best possible product.",
+          "Developing with an custom-built JavaScript framework to build high-performance web pages.",
+          "Leading the project, overseeing a team of developers through code reviews and daily communication.",
+          "Helping multiple teams by doing research, providing guidance for coding and providing solutions to specific issues.",
         ],
       },
     ],
@@ -39,25 +38,25 @@ export const experiences: CompanyExperience[] = [
       {
         startDate: "May 2021",
         endDate: "May 2022",
-        role: "Frontend lead",
+        role: "Frontend Developer",
         modality: "On-site",
         time: "Full-time",
         contents: [
-          "Worked daily with a team of four people to build a complex managment system for a specific type of company.",
-          "Developed primarly with Flutter and Dart, aiming for a multi-platform experience.",
-          "Improved the design of already existing websites, using plain HTML, CSS and JavaScript.",
+          "Worked with Flutter and Dart, aiming for a multi-platform experience.",
+          "Planned, designed and developed the entire frontend for a telecommunications management web application.",
+          "Mantained and improved already existing websites, using plain HTML, CSS and JavaScript.",
         ],
       },
       {
         startDate: "Feb 2023",
         endDate: "May 2023",
-        role: "Frontend developer",
+        role: "Frontend Developer",
         modality: "Remote",
         time: "Full-time",
         contents: [
           "Implemented a thorough API structure refactor to improve app security.",
-          "Diligently maintained and supported the app.",
-          "Continuously enhanced the app to optimize the user experience.",
+          "Optimized the app load speed and overall performance.",
+          "Re-designed the UI/UX of certain parts of the app.",
         ],
       },
     ],
