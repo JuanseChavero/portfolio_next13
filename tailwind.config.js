bc5c3ffc8f17c0a39fbb2b15cbee4085d53c2131
JuanseChavero <juansechavero@gmail.com>
2024-08-21 20:31:45 -0300

    chore: prettier formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,60 +1,60 @@
 /** @type {import('tailwindcss').Config} */
 
-const plugin = require('tailwindcss/plugin');
+const plugin = require("tailwindcss/plugin");
 
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './data/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./data/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
     extend: {
       screens: {
-        xs: '350px',
+        xs: "350px",
       },
       colors: {
-        primary: '#fb7e14',
-        secondary: '#fc8e06',
+        primary: "#fb7e14",
+        secondary: "#fc8e06",
       },
       fontFamily: {
-        sans: ['var(--font-global)'],
-        header: ['var(--font-header)'],
+        sans: ["var(--font-global)"],
+        header: ["var(--font-header)"],
       },
       animation: {
-        'slide-up': 'slideUpAndFade 500ms ease',
-        'slide-right': 'slideRightAndFade 500ms ease',
-        'slide-left': 'slideLeftAndFade 500ms ease',
-        'slide-down': 'slideDownAndFade 500ms ease',
+        "slide-up": "slideUpAndFade 500ms ease",
+        "slide-right": "slideRightAndFade 500ms ease",
+        "slide-left": "slideLeftAndFade 500ms ease",
+        "slide-down": "slideDownAndFade 500ms ease",
       },
       keyframes: {
         slideUpAndFade: {
-          from: { opacity: 0, transform: 'translateY(10px)' },
-          to: { opacity: 1, transform: 'translateY(0)' },
+          from: { opacity: 0, transform: "translateY(10px)" },
+          to: { opacity: 1, transform: "translateY(0)" },
         },
         slideRightAndFade: {
-          from: { opacity: 0, transform: 'translateX(-10px)' },
-          to: { opacity: 1, transform: 'translateX(0)' },
+          from: { opacity: 0, transform: "translateX(-10px)" },
+          to: { opacity: 1, transform: "translateX(0)" },
         },
         slideLeftAndFade: {
-          from: { opacity: 0, transform: 'translateX(10px)' },
-          to: { opacity: 1, transform: 'translateX(0)' },
+          from: { opacity: 0, transform: "translateX(10px)" },
+          to: { opacity: 1, transform: "translateX(0)" },
         },
         slideDownAndFade: {
-          from: { opacity: 0, transform: 'translateY(-10px)' },
-          to: { opacity: 1, transform: 'translateY(0)' },
+          from: { opacity: 0, transform: "translateY(-10px)" },
+          to: { opacity: 1, transform: "translateY(0)" },
         },
       },
     },
   },
   plugins: [
-    require('@headlessui/tailwindcss'),
+    require("@headlessui/tailwindcss"),
 
     // Custom variant for focus OR hover: called "hocus"
     plugin(function ({ addVariant }) {
-      addVariant('hocus', ['&:hover', '&:focus']);
+      addVariant("hocus", ["&:hover", "&:focus-visible"]);
     }),
   ],
 };
